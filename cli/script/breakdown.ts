7714decf1d15b47f0b3b4cb6d9740b5e08985cc2
Richard Hua <rihua@microsoft.com>
2016-07-12 15:56:49 -0700

    Task breakdown for VSTS

---
+++ cli/script/breakdown.ts
@@ -0,0 +1,10 @@
+
+File hashing on RN Android
+File hashing on RN iOS
+File hashing on Cordova Android
+File hashing on Cordova iOS
+Claims verification on RN
+Claims verification on Cordova
+Claims generation on CLI
+Investigate certificates management (optional)
+Buddy testing
